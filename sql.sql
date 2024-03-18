2b112a3f8ce90f225de34fcda90fae11416111be
Lujain23 <91234321+Lujain23@users.noreply.github.com>
2024-03-18 17:23:57 -0400

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,4 +1,5 @@
-CREATE TABLE students (
+--create table
+CREATE TABLE IF NOT EXISTS students (
     student_id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE students (
     enrollment_date DATE
 );
 
+--populate data
 INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
 ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
